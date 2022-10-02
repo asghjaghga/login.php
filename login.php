19fa756ebbe97ba6d6748bd467868b4c93b70037
asghjaghga <86305747+asghjaghga@users.noreply.github.com>
2022-10-02 10:00:17 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE php>
+<head>
+<meta charset="utf-8">
+<title>请输入用户名和密码</title>
+</head>
+<body>
+用户名：<input type="text" id="us"><br>
+密码：<input type="password" id="pass"><br>
+登录：<input type="button" value="登录" id="ok">
+<script type="text/javascript">
+var ok=document.getElementById("ok");
+ok.οnclick=function(){
+var us=document.getElementById("us");
+   var us=document.getElementById("pass");
+   if (us.value&&us.value) {
+    alert("正在登录，请稍后。。。");
+   }else{
+    alert("用户名或密码不能为空");
+   }
+}
+</script>
+</body>
+</html>
